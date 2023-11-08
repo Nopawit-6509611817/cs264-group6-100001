bb27cf3e7a96980348c3a3db180a14f7f2e9972f
methawee_6509611999 <methawee.suk@dome.tu.ac.th>
2023-11-08 17:15:20 +0700

    methawee

--- code1.java
+++ code1.java
@@ -2,7 +2,8 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Karnsiree Suwannapatna 6509611536");
         System.out.println("Kanjanakorn Chawrainak 6509611510");
-	System.out.println("Naphat Sutipornsup 6509490022");
-	System.out.println("Nitikon Kanapang 6509611825")
+	    System.out.println("Naphat Sutipornsup 6509490022");
+	    System.out.println("Nitikon Kanapang 6509611825");
+        System.out.println("methawee suksri 6509611999");
     }
 }
