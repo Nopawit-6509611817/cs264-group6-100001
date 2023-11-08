90fd7330032f10da630caf639cecc58aeb18aa56
Naphat First <naphat.sut@dome.tu.ac.th>
2023-11-08 17:02:52 +0700

    name

--- code1.java
+++ code1.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Karnsiree Suwannapatna 6509611536");
         System.out.println("Kanjanakorn Chawrainak 6509611510");
+	System.out.println("Naphat Sutipornsup 6509490022");
     }
 }
