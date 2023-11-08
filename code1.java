fa3ba98c2ed78f5fed702966809e94182dbe3672
nitikon.kan <nitikon.kan@dome.tu.ac.th>
2023-11-08 17:08:42 +0700

    Name

--- code1.java
+++ code1.java
@@ -3,5 +3,6 @@ public class Main{
         System.out.println("Karnsiree Suwannapatna 6509611536");
         System.out.println("Kanjanakorn Chawrainak 6509611510");
 	System.out.println("Naphat Sutipornsup 6509490022");
+	System.out.println("Nitikon Kanapang 6509611825")
     }
 }
