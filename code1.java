da24ed1ac8b01536343bf783685308d5d7bef7b5
Kanjanakorn Chawrainak <kanjanakorn.cha@dome.tu.ac.th>
2023-11-08 16:37:02 +0700

    iyyha name

--- code1.java
+++ code1.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Karnsiree Suwannapatna 6509611536");
+        System.out.println("Kanjanakorn Chawrainak 6509611510");
     }
 }
