624826a3968ac6ec485881b344e7678887b646b9
karnsiree6509611536 <karnsiree.suw@dome.tu.ac.th>
2023-11-13 17:34:31 +0700

    us4-1

---
+++ project/src/main/java/com/example/us4/Us4Application.java
@@ -0,0 +1,13 @@
+package com.example.us4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Us4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Us4Application.class, args);
+	}
+
+}
